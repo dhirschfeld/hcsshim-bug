4567d9b91b879b8f5d4bdb56d3d7d1bbc008e643
dhirschf <david.hirschfeld@stanwell.com>
2018-09-16 16:24:52 +1000

    Clean up build script

--- build.ps1
+++ build.ps1
@@ -1,21 +1,7 @@
 
 $isolation = 'process'
 
-$image = "miniconda-core-1809"
-Write-Host "################################################################################"
-Write-Host "# Building image '$image'"
-Write-Host "################################################################################"
-docker build --force-rm --no-cache --isolation=$isolation -t $image .\miniconda 2>&1 | Tee-Object "$image.log"
-if ($LastExitCode -ne 0) {
-    Write-Host '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
-    Write-Host "# Building image '$image' failed with exit code $LastExitCode"
-    Write-Host '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
-    Exit $LastExitCode
-}
-docker tag $image "${image}:${version}"
-Write-Host "`n`n"
-
-$images = 'repro-base','repro'
+$images = 'miniconda','repro-base','repro'
 ForEach ($image in $images) {
     Write-Host "################################################################################"
     Write-Host "# Building image '$image'"
